cf024ea6c02d33d3c36b342beffe55c42724a221
fahad-islam1 <fahadch127127@gmail.com>
2024-07-12 12:00:35 +0500

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-// const fs = require('fs');
 const fs =require("fs")
+
+// two methods for creating file
+
+
+fs.writeFile('assignments.txt', 'This is my file system assignment ', (error)=>{
+    if(error) throw error;
+    console.log('File has been created!');
+})
 fs.writeFileSync("helo.js","somethning")
+
+
+
+// node mon
